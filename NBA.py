6e803b9198cc492fb9bb06eda11bfab9ac59c6eb
bhufnagel <Brodinator@Live.com>
2021-12-07 17:09:06 -0600

    Added functions for getting team roster and exporting data

--- NBA.py
+++ NBA.py
@@ -1,3 +1,7 @@
+import requests
+import json
+import pandas
+
 teams = {'GSW': 1610612744,
         'PHX':  1610612756,
         'BKN':  1610612751,
@@ -28,3 +32,67 @@ teams = {'GSW': 1610612744,
         'OKC':  1610612760,
         'ORL':  1610612753,
         'DET':  1610612765}
+
+
+
+def teamroster():
+    team = input("\nWhich team, bossman?\n")
+    if team in teams:    
+        teamID = teams.get(team)
+        url = "https://stats.nba.com/stats/commonteamroster?LeagueID=00&Season=2021-22&TeamID=" + str(teamID)
+
+        headers = {
+            'sec-ch-ua': '"Opera GX";v="81", " Not;A Brand";v="99", "Chromium";v="95"',
+            'sec-ch-ua-mobile': '?0',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36 OPR/81.0.4196.61',
+            'Accept': 'application/json, text/plain, */*',
+            'x-nba-stats-token': 'true',
+            'Referer': 'https://www.nba.com/',
+            'x-nba-stats-origin': 'stats',
+            'sec-ch-ua-platform': '"Windows"',
+        }
+
+        response = requests.request("GET", url, headers=headers, timeout=5)
+        teamdata = response.json()
+
+        df = pandas.json_normalize(teamdata['resultSets'])
+        print(df.head())
+
+        export(df, team + "_roster")
+    else:
+        print("Idk who that is...")
+        userInput()
+
+
+
+def export(df, name):
+    yn = input("\nDo you want to save this data?\n")
+    if yn == "yes" or yn == "y" or yn == "Yes" or yn == "Y":
+        df.to_csv(name + '.csv', index = False)
+        print("Data saved to file \"" + name + ".csv\"")
+        userInput()
+    if yn == "no" or yn == "n" or yn == "No" or yn == "N":
+        userInput()
+    else:
+        print("Huh?\n")
+        export(df, name)
+
+
+
+def userInput():
+    command = input("\nHow may I help you, mush?\n")
+    if command == "team roster":
+        teamroster()
+    if command == "help":
+        print("\nCommands:")
+        print("\"team roster\" will return the roster for a team of your choice")
+        userInput()
+    else:
+        print("what? (type \"help\" for a list of commands)")
+        userInput()
+
+
+
+print("\n~~~ Hello, mush. Welcome to the MUSH MASTER 3000 ~~~")
+userInput()
+
